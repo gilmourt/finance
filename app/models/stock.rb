880d8d3ba850a5f207593298ec94939649ae0dc8
Timothy Gilmour <tim.g.gilmour@gmail.com>
2020-05-02 15:07:41 -0400

    Added partial to render table

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,5 @@
 class Stock < ApplicationRecord
+    validates :ticker, :uniqueness => true
     validate :ticker_must_be_valid, :on => :create 
     def ticker_must_be_valid
         begin 
--- db/migrate/20200306032729_create_stocks.rb
+++ db/migrate/20200306032729_create_stocks.rb
@@ -1,7 +1,7 @@
 class CreateStocks < ActiveRecord::Migration[5.1]
   def change
     create_table :stocks do |t|
-      t.string :ticker
+      t.string :ticker, unique: true
     end
   end
 end
